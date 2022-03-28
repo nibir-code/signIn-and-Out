ba120dc80c1263bb9328fb82161f375ce6ab8b56
nibir-code <mubtashimfuadnibir@gmail.com>
2022-03-29 04:11:43 +0600

    commit message

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import * as firebase from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup }  from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup}  from "firebase/auth";
 import firebaseConfig from "./firebase config";
 import 'firebase/compat/auth';
 import { useState } from "react";
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
 
 
 firebase.initializeApp(firebaseConfig)
@@ -13,6 +16,7 @@ function App() {
     isSignedIn:false,
     name :'',
     mail:'' ,
+    password:'',
     photo:''
   })
   const provider = new GoogleAuthProvider();
@@ -45,10 +49,55 @@ function App() {
           isSignedOut:false,
           name:'',
           email:'',
-          photo:''
+          photo:'',
+        
         }
         setUser(signedOutUser);
       })
+    }
+    const handleBlur =(e)=>{
+      debugger;
+     let isFormValid = true;
+      if (e.target.name==='email') {
+        isFormValid = /\S+@\S+\.\S+/.test(e.target.value);
+        
+        
+      }
+      if (e.target.value=== 'password') {
+        const isPasswordValid = e.target.value.length>6;
+        const passwordHasNumber= /\d{1}/.test(e.target.value);
+        isFormValid=isPasswordValid && passwordHasNumber;
+      }
+      if (isFormValid){
+        const newUserInfo ={...user};
+        newUserInfo[e.target.name]= e.target.value;
+        setUser(newUserInfo);
+
+      }
+    }
+    const handleSubmit =(e)=>{
+      console.log(user.email, user.password);
+      if(user.email && user.password){
+        //console.log('submitting');
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth, user.email, user.password)
+          .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            // ...
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+            console.log(errorCode,errorMessage);
+          });
+        
+
+
+      }
+      e.preventDefault();
+
     }
 
   return (
@@ -59,14 +108,32 @@ function App() {
        {
          user.isSignedIn &&
          <div>
-            <p>WELCOME {user.name}</p>
-            <p>Email: {user.mail}</p>
+            <p>WELCOME  <b> {user.name}</b></p>
+            <p>Email:  <b>{user.mail}</b></p>
             <img src={user.photo} alt="" />
+            <AccessAlarmIcon></AccessAlarmIcon>
          </div> 
          
+         
        }
+       <h1>Our Own Authentication</h1>
+      <form onSubmit={handleSubmit}>
+        <h1>Our own Authentication</h1>
+        <p>name : {user.name}</p>
+        <p>Email:{user.email}</p>
+        <p>password :{user.password}</p>
+
+        <input type="text" name="name" onBlur={handleBlur} placeholder='your name' required/>
+        <br />
+        <input type="text" name="email" onBlur={handleBlur} placeholder='your email address' required/>
+       <br/>
+        <input type="password" name="password" onBlur={handleBlur} placeholder='your password' required/>
+       <br/>
+        <input type="button" value="submit" />
+     </form>
     </div>
   );
+  
 }
 
 export default App;
